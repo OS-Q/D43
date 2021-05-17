4ebdee198a0652bd164613c3030bf8fef5b49d36
qitas <qitas@qitas.cn>
2021-05-17 09:55:17 +0800

    fix

---
+++ scripts/debug.py
@@ -0,0 +1,16 @@
+Import("env")
+import shutil
+import os
+
+env.Append(LINKFLAGS=["--specs=nano.specs"])
+# env.Append(LINKFLAGS=["--specs=nosys.specs", "--specs=nano.specs"])
+
+def after_build(source, target, env):
+	isExists=os.path.exists('build')
+	if not isExists:
+		os.mkdir('build')
+	shutil.copy(firmware_source, 'build/debug.bin')
+
+env.AddPostAction("buildprog", after_build)
+
+firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
